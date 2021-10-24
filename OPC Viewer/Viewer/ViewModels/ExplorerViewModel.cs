ca9672eac84b14db29484be3ccd1bd589a66fcc2
James Dingle <ding_jimmy@hotmail.com>
2021-10-24 14:43:22 +0100

    Add basic commanding logic to ExplorerViewModel

--- OPC Viewer/Viewer/ViewModels/ExplorerViewModel.cs
+++ OPC Viewer/Viewer/ViewModels/ExplorerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,17 +12,63 @@ namespace Viewer.ViewModels
 {
     public class ExplorerViewModel : BindableBase
     {
-        public ObservableCollection<TreeItemViewModel> TreeRoot { get; set; }
+        /// <summary>
+        /// Gets a collection of root items.
+        /// </summary>
+        public ObservableCollection<TreeItemViewModel> Items { get; private set; } = new ObservableCollection<TreeItemViewModel>();
 
-        public ICommand ConnectCommand { get; set; }
+        /// <summary>
+        /// Gets a command which when executed begins the process of connecting to a server.
+        /// </summary>
+        public ICommand ConnectCommand => _ConnectCommand ??= new DelegateCommand(async () => await ConnectAsync(), CanConnect);
+        private DelegateCommand _ConnectCommand;
 
-        public ICommand DisconnectCommand  { get; set; }
+        /// <summary>
+        /// Gets a command which when executed disconnects from the current server.
+        /// </summary>
+        public ICommand DisconnectCommand => _DisconnectCommand ??= new DelegateCommand(async () => await DisconnectAsync(), CanDisconnect);
+        private DelegateCommand _DisconnectCommand;
 
-        public ICommand RefreshCommand { get; set; }
+        /// <summary>
+        /// Gets a command which when executed refreshes the tree.
+        /// </summary>
+        public ICommand RefreshCommand => _RefreshCommand ??= new DelegateCommand(async () => await RefreshAsync(), CanRefresh);
+        private DelegateCommand _RefreshCommand;
+
 
         public ExplorerViewModel()
         {
 
+        }
+
+        public bool CanConnect()
+        {
+            return true;
+        }
+
+        public async Task ConnectAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        public bool CanDisconnect()
+        {
+            return true;
+        }
+
+        public async Task DisconnectAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        public bool CanRefresh()
+        {
+            return true;
+        }
+
+        public async Task RefreshAsync()
+        {
+            await Task.CompletedTask;
         }
     }
 }
