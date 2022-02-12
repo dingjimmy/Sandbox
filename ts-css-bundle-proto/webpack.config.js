e7a10799d7e3188f1d3ba4305094fc42b87c9efd
James Dingle <jamesd@amdigital.co.uk>
2022-02-12 00:49:15 +0000

    delete stale generated files before each build

--- ts-css-bundle-proto/webpack.config.js
+++ ts-css-bundle-proto/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'wwwroot'),
+        clean: true
     },
     module: {
         rules: [
