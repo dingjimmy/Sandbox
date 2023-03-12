d14589105b3dbb0072d849fa464b113628900b77
James Dingle <ding_jimmy@hotmail.com>
2023-03-12 22:37:19 +0000

    refactor: import component folders
    Add index.tsx files for each component, so consumers can import component just by referencing the components folder name. This dramatically cleans-up import statements, making code easier to read.

--- imgzoom/imgzoom-viewer/src/index.tsx
+++ imgzoom/imgzoom-viewer/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
-import Viewer from './components/Viewer/Viewer';
+import App from './components/App';
+import Viewer from './components/Viewer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
