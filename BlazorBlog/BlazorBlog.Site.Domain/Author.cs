aeab0e7d55de62a8c56a1c0291e550aadd07b851
James Dingle <dingjimmy@users.noreply.github.com>
2024-01-14 13:55:30 +0000

    feat(blazor-blog): add article entity and associated classes (#5)
    wip[blazorblog]: add domain class library
    
    wip[blazor-blog]: add article entity & supporting classes
    
    wip[blazor-blog]: TextBlock entity
    
    Revert "wip[blazor-blog]: TextBlock entity"
    
    This reverts commit 1cfa07758b75997b56a5e51d49d62d0a3322a75c.

---
+++ BlazorBlog/BlazorBlog.Site.Domain/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Site.Domain
+{
+    public class Author
+    {
+        public string Name { get; }
+
+        public Author(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException($"An authors {nameof(name)} cannot be null or whitespace.", nameof(name));
+            }
+
+            Name = name;
+        }
+    }
+}
