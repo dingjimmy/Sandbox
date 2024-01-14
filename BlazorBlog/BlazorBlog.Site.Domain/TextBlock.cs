1cfa07758b75997b56a5e51d49d62d0a3322a75c
James Dingle <ding_jimmy@hotmail.com>
2024-01-14 02:58:40 +0000

    wip[blazor-blog]: TextBlock entity

---
+++ BlazorBlog/BlazorBlog.Site.Domain/TextBlock.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBlog.Site.Domain
+{
+    public class TextBlock : IContentBlock
+    {
+        public string Heading { get; set; }
+
+        public string Content { get; set; }
+    }
+}
